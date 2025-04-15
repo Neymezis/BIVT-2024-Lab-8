625e3b025d2e00d828bbdfe505ae7c53e26746de
Neymezis <m2405004@edu.misis.ru>
2025-04-15 18:21:58 +0300

    Update Purple_4.cs

--- Purple_4.cs
+++ Purple_4.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace Lab_8
     {
         private string _output;
         public string Output => _output;
-        public (string, char)[] _codes;
+        private (string, char)[] _codes;
         public (string, char)[] Codes
         {
             get
